6e04c566c7798c6ea51f2b78c94686738e802687
jeff szajner <jszajner@yahoo.com>
2016-07-16 20:31:49 -0600

    refactor-completed

--- fizzbuzz-refactor.js
+++ fizzbuzz-refactor.js
@@ -6,20 +6,20 @@ function validateInput() {
         var inputNumber = prompt("Please enter 1 digit or more.");
         userInputCorrectFlag = false;
     }
-    //no spaces
+    //no spaces (if the number of spaces is larger then zero then do not run code.)
     while (inputNumber.indexOf(' ') > 0) {
-        var inputNumber = prompt("Please don't enter spaces. Try anumber!");
+        var inputNumber = prompt("Please don't enter spaces. Try a number!");
         userInputcorrectFlag = false;
 
     }
     //the limit is a number (if the rounded value of your number (MATH.FLOOR(inputNumber)) is the same with the initial number (imputNumber),it means that the number (inputNumber) was integer in the first place)
-    While(Math.floor(inputNumber) != inputNumber) {
-            var inputNumber = prompt("Your upper limit was not a number.Set it again.");
-            //change the user input flag to false
-            user userInputcorrectFlag = false;
-        }
-        //PART 3 => if the limit is valid, then play the game
-    If(userInputCorrectFlag = true) {
+    while (Math.floor(inputNumber) != inputNumber) {
+        var inputNumber = prompt("Your upper limit was not a number.Set it again.");
+        //change the user input flag to false
+        userInputcorrectFlag = false;
+    }
+    //PART 3 => if the limit is valid, then play the game
+    if (userInputCorrectFlag = true) {
         playFizzBuzz(inputNumber);
     }
 }
@@ -50,7 +50,7 @@ function checkFizzAndBuzz(currentNumber) {
 }
 //Then use the functions//
 
-$(document).ready(function)() {
+$(document).ready(function () {
 
     //PART 1 => get the upper limit from the input
     var limit = prompt("Please set the upper limit to play FizzBuzz.");
@@ -58,4 +58,4 @@ $(document).ready(function)() {
     //PART 2 => Starting the validation
     validateInput(limit);
 
-}
+});
